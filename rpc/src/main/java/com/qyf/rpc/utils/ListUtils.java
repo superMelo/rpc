91260da5351afa1d89310ef7b15609f3aab5a77a
superMelo <774876970@qq.com>
2021-01-22 09:30:51 +0800

    修改逻辑

---
+++ rpc/src/main/java/com/qyf/rpc/utils/ListUtils.java
@@ -0,0 +1,13 @@
+package com.qyf.rpc.utils;
+
+import java.util.List;
+
+public class ListUtils {
+
+    public static boolean emtry(List list){
+        if (list == null || list.size() == 0){
+            return true;
+        }
+        return false;
+    }
+}
