5c210a98b7ccb9b00cc9fa48d6703cb5e280f3db
superMelo <774876970@qq.com>
2021-01-29 17:28:41 +0800

    添加动态修改配置

--- rpc/src/main/java/com/qyf/rpc/config/common/RedisConfig.java
+++ rpc/src/main/java/com/qyf/rpc/config/common/RedisConfig.java
@@ -1,24 +1,25 @@
-package com.qyf.rpc.register.redis;
+package com.qyf.rpc.config.common;
 
 
-import org.springframework.beans.factory.annotation.Value;
+import com.qyf.rpc.annotion.ConfigBean;
+import com.qyf.rpc.annotion.RpcConfig;
+import com.qyf.rpc.annotion.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import redis.clients.jedis.Jedis;
 
-@Configuration
+@RpcConfig
 public class RedisConfig {
 
 
-    @Value("${redis.host}")
-    private String host;
 
-    @Value("${redis.port}")
+    @Value("redis.host")
+    private String host;
+    @Value("redis.port")
     private int port;
 
-
-    @Bean
-    public Jedis getJedis(){
+    @ConfigBean
+    public Jedis jedis(){
         Jedis jedis = new Jedis(host, port);
         return jedis;
     }
