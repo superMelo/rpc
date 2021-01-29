a8dc832e9c63f96be45d0cdf9b964a3797120f8f
superMelo <774876970@qq.com>
2021-01-29 17:09:39 +0800

    添加动态配置注解

---
+++ rpc/src/main/java/com/qyf/rpc/annotion/ConfigBean.java
@@ -0,0 +1,10 @@
+package com.qyf.rpc.annotion;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface ConfigBean {
+}
---
+++ rpc/src/main/java/com/qyf/rpc/annotion/RpcConfig.java
@@ -0,0 +1,12 @@
+package com.qyf.rpc.annotion;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface RpcConfig {
+}
